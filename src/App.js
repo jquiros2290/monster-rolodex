b7408ba71f43dff1f6bbc1dd2b94bc4b649ff72d
Justin <justin.quiros@monkedia.com>
2019-08-05 14:39:07 -0500

    Monsters are now searchable

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
 import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: '',
     };
+  }
+
+  handleChange = e => {
+    this.setState({searchField: e.target.value});
   }
 
   componentDidMount() {
@@ -18,9 +24,13 @@ class App extends Component {
   }
 
   render() {
+    const {monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter((monster) => { return monster.name.toLowerCase().includes(searchField.toLowerCase()) });
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <SearchBox placeholder="Search Monsters" handleChange={this.handleChange} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
