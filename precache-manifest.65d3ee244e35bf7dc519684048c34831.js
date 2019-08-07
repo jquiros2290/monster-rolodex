e56c75631e7abf5fae06759f5967443844a48573
Justin <justin.quiros@monkedia.com>
2019-08-07 06:38:22 -0500

    Updates

---
+++ precache-manifest.65d3ee244e35bf7dc519684048c34831.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df487663a8e150f8b3be7ef525623d81",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "16b16870e3f9bc40a208",
+    "url": "/monster-rolodex/static/css/main.2adefd65.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "16b16870e3f9bc40a208",
+    "url": "/monster-rolodex/static/js/main.6e349c07.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
